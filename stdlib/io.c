a5e34c1738f8c8007e263b6e83f7fd994dd86c6e
Patrick Flynn <patrickf4664@protonmail.com>
2021-09-15 09:28:27 -0400

    Added stdlib functions for printing floating-point types

--- stdlib/io.c
+++ stdlib/io.c
@@ -3,6 +3,9 @@
 extern void syscall_str4(int code, int fd, const char *buf, int size);
 extern int strlen(const char *line);
 
+// Needed for the floating point stuff
+int precision = 6;
+
 void printInt(int num)
 {
     // For some reason, weird things happen with just 0
@@ -165,4 +168,65 @@ void printf(const char *line, int64_t arg1, int64_t arg2, int64_t arg3, int64_t 
     }
     
     syscall_str4(1, 1, (char *)buffer, index);
+}
+
+void printDouble(double num)
+{
+   char flt_num[64];
+   int i = 0;
+   
+   if (num < 0.0) {
+       flt_num[i++] = '-';
+       num *= -1;
+   }
+   
+   int whole = num;     // Gets us the whole number part
+   num -= whole;        // Gets us the decimal part
+   
+   // Print the whole number part
+   if (whole == 0) {
+       flt_num[i++] = '0';
+   } else {
+       // Count the number of digits
+       int digits = 0;
+       int whole2 = whole;
+       while (whole2 != 0) {
+           whole2 /= 10;
+           ++digits;
+       }
+       
+       char buf[digits];
+       int j = 0;
+       while (whole != 0) {
+           int digit = whole % 10;
+           whole /= 10;
+           buf[j++] = digit + '0';
+       }
+       
+       for (int ii = digits - 1; ii>=0; ii--) flt_num[i++] = buf[ii];
+   }
+   
+   flt_num[i++] = '.';
+   
+   // Print the decimal part
+   for (int j = 0; j<precision; j++) {
+       num *= 10.0;
+       int digit = num;
+       flt_num[i++] = digit + '0';
+       num -= digit;
+   }
+   
+   flt_num[i++] = '\0';
+   
+   syscall_str4(1, 1, (char *)flt_num, i);
+}
+
+void printFloat(float num)
+{
+    printDouble(num);
+}
+
+void setPrecision(int p)
+{
+    precision = p;
 }
