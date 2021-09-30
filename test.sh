73e102292de43d500db2b23baa278d3c04d0b3be
Patrick Flynn <patrickf4664@protonmail.com>
2021-09-30 10:18:57 -0400

    Added tests for OOP support so far

--- test.sh
+++ test.sh
@@ -63,6 +63,7 @@ run_test 'test/enum/*.ok' 'sys' $flags
 run_test 'test/struct/*.ok' 'sys' $flags
 run_test 'test/float/*.ok' 'sys' $flags
 run_test 'test/str/*.ok' 'sys' $flags
+run_test 'test/class/*.ok' 'sys' $flags
 
 echo ""
 echo "$test_count tests passed successfully."
